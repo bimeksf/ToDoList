32fbed7a00e43a89584958ff8f7b6e8c3dde7363
bimeksf <d3disek@gmail.com>
2025-04-10 18:13:11 +0200

    fix: enable dayjs import and update deadline handling in Task component

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect ,useRef } from "react";
-//import dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import React from 'react';
 export default function Task({ task, onToggle, handleDelete, onEdit }) {
   const [isEditing, setIsEditing] = useState(false);
@@ -11,9 +11,9 @@ export default function Task({ task, onToggle, handleDelete, onEdit }) {
 
 
 
-  //const afterDeadline= dayjs(task.deadline).isBefore(dayjs())
+  const afterDeadline= dayjs(task.deadline).isBefore(dayjs())
 
-  //const closeDeadline = dayjs(task.deadline).diff(dayjs() , "hour" ) <=24
+  const closeDeadline = dayjs(task.deadline).diff(dayjs() , "hour" ) <=24
 
 
 
@@ -44,8 +44,9 @@ export default function Task({ task, onToggle, handleDelete, onEdit }) {
 
 
   return (
-    <>
-      <li className={task.completed ? "underline bg-blue-300 rounded-md m-4" : "bg-blue-300 rounded-md m-4"} onDoubleClick={()=>setIsEditing(true) }>
+    <div className="flex">
+   { closeDeadline && !afterDeadline && <svg className="w-20 animate-pulse " xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 40 40"><path fill="currentColor" d="M37.874 37.2H2.132a1.742 1.742 0 0 1-1.501-.866a1.707 1.707 0 0 1-.001-1.729L18.507 3.663a1.728 1.728 0 0 1 2.991-.004l17.833 30.869a1.733 1.733 0 0 1-1.457 2.672zM20.001 3.8a.738.738 0 0 0-.628.363L1.496 35.104a.699.699 0 0 0-.001.727a.739.739 0 0 0 .637.369h35.742a.741.741 0 0 0 .732-.73a.787.787 0 0 0-.134-.429L20.634 4.163a.738.738 0 0 0-.633-.363z"/><path fill="currentColor" d="M20.002 27.948a.5.5 0 0 1-.5-.5V9.864a.5.5 0 0 1 1 0v17.584a.5.5 0 0 1-.5.5zm-.004 3.688a.5.5 0 0 1-.5-.5V29.78a.5.5 0 0 1 1 0v1.355a.5.5 0 0 1-.5.501z"/></svg>}
+   <li className={`${task.completed ? "underline" : ""} bg-blue-300 rounded-md m-4 ${afterDeadline ? "bg-red-200" : ""}`} onDoubleClick={()=>setIsEditing(true)}>
         {isEditing ? (
           <div className='grid grid-cols-2 grid-rows-4 gap-4'>
 
@@ -100,6 +101,6 @@ export default function Task({ task, onToggle, handleDelete, onEdit }) {
   }} aria-label="Cancel changes" className="bg-red-500 px-4 py-2 hover:scale-105 rounded-md text-md text-white font-medium">Cancel</button>
         </div>
       ) }
-    </>
+    </div>
   );
 }
